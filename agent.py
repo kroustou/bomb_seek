3e91c7709823f25730d9575a290289b0588b7b81
Stavros <kroustou@gmail.com>
2012-09-01 14:12:57 +0300

    change the colors of the agents

--- agent.py
+++ agent.py
@@ -2,6 +2,10 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 import random
 
+from astar import AStar
+from utils import neighbours
+
+
 class Agent():
     def __init__(self, position, qp, parent, is_scout=False):
         (self.x, self.y) = position
@@ -9,10 +13,10 @@ class Agent():
         self.path = [position]
         self.is_scout = is_scout
         self.parent = parent
-        self.color = Qt.yellow if self.is_scout  else Qt.green
+        self.color = Qcolor(0, 96, 109, 255) if self.is_scout  else Qcolor(0, 168, 109, 255)
         self.draw_path = False
         self.bomb_location = False
-        self.path_to_bomb = []
+        self.path_to_bomb = []  
         self.exchanges = 0
 
     def move(self):
@@ -68,20 +72,17 @@ class Agent():
             # check if agents meet each other
             for agent in self.parent.agents:
                 if (agent.x, agent.y) in neighbours((self.x, self.y)):
-                    try:
-                        moves.remove((self.x, self.y))
-                    except:
-                        pass
                     self.exchanges += 1
+                    agent.path += self.path
+                    self.path += agent.path
+                    self.path = list(set(self.path))
+                    agent.path = list(set(agent.path))
                     if (agent.is_scout) and (self.bomb_location) and (not agent.bomb_location):
                         agent.bomb_location = self.bomb_location
-                        agent.path += self.path
                         return True
                     else:
                         if self.bomb_location and not agent.bomb_location:
-                            for p in agent.path:
-                                if p not in self.path:
-                                    self.path.append(p)
+                            
                             agent.bomb_location = self.bomb_location
                             agent.color = Qt.blue
 
@@ -124,83 +125,3 @@ class Agent():
         if self.x > 0 and not self.parent.is_wall(self.x - 1, self.y):
             moves.append((self.x - 1, self.y))
         return moves
-
-
-def neighbours((x, y), including_self=False):
-    if including_self:
-        return [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-    else:
-        return [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1), (x, y)]
-
-
-
-class Node:
-    def __init__(self, position, end, parent=None):
-        self.position = position
-        self.parent = parent
-        self.x = position[0]
-        self.y = position[1]
-        self.end = end
-        if not parent:
-            self.g = 0
-        else:
-            self.g = parent.g + 1
-        self.h = abs(self.x - self.end[0]) + (self.y - self.end[1])
-        self.f = self.g + self.h
-
-
-class AStar():
-    def __init__(self):
-        print 'astar init'
-        self.open_list = []
-        self.closed_list = []
-
-    def find_path(self, start, end, known_region):
-        print 'finding path'
-        current = Node(start, end)
-        self.open_list.append(current)
-        self.map = known_region
-        
-        while self.open_list:
-            #find the neighbours of the last item in list 
-            self.open_list += self.neighbours(current, end)
-            self.open_list.remove(current)
-            self.closed_list.append(current)
-
-            shortest_f = self.open_list[0]
-            for node in self.open_list:
-                if node.f < shortest_f.f:
-                    shortest_f = node
-            current = shortest_f
-            if current.position in neighbours(end):
-                print 'target found:', end
-                break
-
-        path_nodes = [current]
-        while path_nodes[-1].position != start:
-            path_nodes.append(path_nodes[-1].parent)
-        path = []
-        for node in path_nodes:
-            path.append(node.position)
-        print 'path found:', path
-        return path
-
-    def neighbours(self, node, end):
-        candidates = []
-        (x,y) = node.position
-        neighbours = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
-        for n in neighbours:
-            if n in self.map:
-                candidate = Node(n, end, node)
-                put = True
-                for i in self.closed_list:
-                    if i.position == candidate.position:
-                        put = False 
-                        break
-                for i in self.open_list:
-                    if i.position == candidate.position:
-                        put = False 
-                        break
-                if put:
-                    candidates.append(candidate)
-        return candidates
